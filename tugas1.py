d544562c2b16ad0c35b5db9c034a2a961e7f9101
annisacahyawardani <annisacahyawardani04@gmail.com>
2024-03-23 11:48:33 +0700

    dasar pemograman

---
+++ tugas1.py
@@ -0,0 +1,21 @@
+#Nama  : Annisa Cahya Wardani
+#NIM   : 19235134
+#Kelas : 19. 1A. 24
+
+
+print("==============================")
+l = int(input("Masukan lebar segiempat = "))
+p = int(input("Masukan panjang segiempat = "))
+luas_segiempat = p * l
+print("luas segiempat adalah", str(luas_segiempat))
+
+print("==============================")
+a = int(input("Masukan alas segitiga = "))
+t = int(input("Masukan tinggi segitiga = "))
+luas_segitiga = 1/2 * a * t
+print("Luas segitiga adalah", str(luas_segitiga))
+
+print("===============================")
+r = int(input("Masukan jari-jari lingkaran ="))
+luas_lingkaran = 22/7 * r * r
+print("Luas lingkaran adalah", str(luas_lingkaran))
