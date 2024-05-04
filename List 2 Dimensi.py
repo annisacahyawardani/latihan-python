86468a80c1c4f7fd631ce1645ef2ddc806d6ffe7
annisacahyawardani <annisacahyawardani04@gmail.com>
2024-05-04 15:36:49 +0700

    List Dua Dimensi

---
+++ List 2 Dimensi.py
@@ -0,0 +1,7 @@
+list = [1,2,3,4,5,6,7,8,9,0]
+list1 = ("Baris", "Kolom", "Pertama", "Kedua", "Ketiga", "Keempat", "Adalah", ":")
+print(list1[0], list1[2], list1[1], list1[2], list1[6], list1[7], "\n", list[0])
+print(list1[0], list1[2], list1[1], list1[3], list1[6], list1[7], "\n", list[1])
+print(list1[0], list1[2], list1[1], list1[4], list1[6], list1[7], "\n", list[2])
+print(list1[0], list1[4], list1[1], list1[4], list1[6], list1[7], "\n", list[8])
+print(list1[0], list1[5], list1[1], list1[2], list1[6], list1[7], "\n", list[9])
